68b634d96c18c1769f8f1de7345dd470deedb892
dicetheman <idongesitumoh9@gmail.com>
2026-03-06 12:21:49 +0000

    Implement feature Logging

--- api/main.py
+++ api/main.py
@@ -7,6 +7,10 @@ from fastapi import FastAPI
 
 from api.routers.journal_router import router as journal_router
 
+#my imports
+import logging
+import sys
+
 
 # TODO: Setup basic console logging
 # Hint: Use logging.basicConfig() with level=logging.INFO
@@ -16,5 +20,15 @@ from api.routers.journal_router import router as journal_router
 # 3. Add console handler
 # 4. Test by adding a log message when the app starts
 
+logging.basicConfig(
+    level = logging.INFO,
+    #format = "%(asctime)s [%(name)s] %(levelname)s: %(message)s",
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+#logger = logging.getLogger(__name__)
+logging.info(" The API is starting up ")
+
 app = FastAPI(title="Journal API", description="A simple journal API for tracking daily work, struggles, and intentions")
 app.include_router(journal_router)
+
+
