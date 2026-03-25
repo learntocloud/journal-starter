0db01bb39909a4774264691f5ed6420fd45254ad
Soltime5476 <129920421+Soltime5476@users.noreply.github.com>
2026-03-25 15:42:23 +0000

    Implement feature logging-setup

--- api/main.py
+++ api/main.py
@@ -3,10 +3,11 @@ from dotenv import load_dotenv
 
 load_dotenv(override=True)
 
+import logging
+
 from fastapi import FastAPI
 
 from api.routers.journal_router import router as journal_router
-
 
 # TODO: Setup basic console logging
 # Hint: Use logging.basicConfig() with level=logging.INFO
@@ -15,6 +16,16 @@ from api.routers.journal_router import router as journal_router
 # 2. Set level to logging.INFO
 # 3. Add console handler
 # 4. Test by adding a log message when the app starts
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+handler = logging.StreamHandler()
+logger.addHandler(handler)
+iso_8601 = '%Y-%m-%dT%H:%M:%S'
+fmt = "\033[34mCUSTOM\033[0m:   {asctime}  {name} - {message} ({filename}:{lineno})"
+formatter = logging.Formatter(fmt, iso_8601, style='{')
+handler.setFormatter(formatter)
+logger.info("App is starting...")
 
 app = FastAPI(title="Journal API", description="A simple journal API for tracking daily work, struggles, and intentions")
 app.include_router(journal_router)
